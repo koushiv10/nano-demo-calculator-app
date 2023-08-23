e4f19acc86b33ac25f16b0137c1469ed73b7e82e
Koushi Vesangi <91794165+koushiv10@users.noreply.github.com>
2023-08-23 22:35:05 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,29 +4,29 @@ app = Flask(__name__)
 
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return 'Hello world!', 200
+    return "Hello world!", 200
 
 @app.route('/calculator/add', methods=['POST'])
 def add():
     data = request.get_json()
     if 'first' in data and 'second' in data:
-        first_number = data['first']
-        second_number = data['second']
-        result = first_number + second_number
+        first = data['first']
+        second = data['second']
+        result = first + second
         return jsonify({'result': result}), 200
     else:
-        return 'Missing parameters', 400
+        return "Bad Request", 400
 
 @app.route('/calculator/subtract', methods=['POST'])
 def subtract():
     data = request.get_json()
     if 'first' in data and 'second' in data:
-        first_number = data['first']
-        second_number = data['second']
-        result = first_number - second_number
+        first = data['first']
+        second = data['second']
+        result = first - second
         return jsonify({'result': result}), 200
     else:
-        return 'Missing parameters', 400
+        return "Bad Request", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
