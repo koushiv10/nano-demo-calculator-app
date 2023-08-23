54c0a206aafe4679627cd4afd9f8071ab304ff7e
Koushi Vesangi <91794165+koushiv10@users.noreply.github.com>
2023-08-23 22:12:24 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,20 +9,24 @@ def greeting():
 @app.route('/calculator/add', methods=['POST'])
 def add_numbers():
     data = request.get_json()
-    first_number = data['first']
-    second_number = data['second']
-    
-    result = first_number + second_number
-    return jsonify({'result': result}), 200
+    if 'first' in data and 'second' in data:
+        first_number = data['first']
+        second_number = data['second']
+        result = first_number + second_number
+        return jsonify({'result': result}), 200
+    else:
+        return jsonify({'error': 'Both numbers are required'}), 400
 
 @app.route('/calculator/subtract', methods=['POST'])
 def subtract_numbers():
     data = request.get_json()
-    first_number = data['first']
-    second_number = data['second']
-    
-    result = first_number - second_number
-    return jsonify({'result': result}), 200
+    if 'first' in data and 'second' in data:
+        first_number = data['first']
+        second_number = data['second']
+        result = first_number - second_number
+        return jsonify({'result': result}), 200
+    else:
+        return jsonify({'error': 'Both numbers are required'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
