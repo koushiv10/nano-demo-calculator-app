16005ae93c09bf32b7a1bcfd1221251e7e3bb710
Koushi Vesangi <91794165+koushiv10@users.noreply.github.com>
2023-08-23 22:29:29 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,21 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/calculator/greeting', methods=['GET'])
+@app.route('/caluclator/greeting', methods=['GET'])
 def greeting():
     return "Hello world!", 200
 
 @app.route('/calculator/add', methods=['POST'])
 def add_numbers():
     data = request.get_json()
-    if not data or 'first' not in data or 'second' not in data:
-        return jsonify({'error': 'Both first and second numbers are required'}), 400
-    
-    try:
-        first_number = float(data['first'])
-        second_number = float(data['second'])
-    except ValueError:
-        return jsonify({'error': 'Invalid numbers provided'}), 400
+    first_number = data['first']
+    second_number = data['second']
     
     result = first_number + second_number
     return jsonify({'result': result}), 200
@@ -24,14 +18,8 @@ def add_numbers():
 @app.route('/calculator/subtract', methods=['POST'])
 def subtract_numbers():
     data = request.get_json()
-    if not data or 'first' not in data or 'second' not in data:
-        return jsonify({'error': 'Both first and second numbers are required'}), 400
-    
-    try:
-        first_number = float(data['first'])
-        second_number = float(data['second'])
-    except ValueError:
-        return jsonify({'error': 'Invalid numbers provided'}), 400
+    first_number = data['first']
+    second_number = data['second']
     
     result = first_number - second_number
     return jsonify({'result': result}), 200
