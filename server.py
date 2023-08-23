7c1858db439516f7fb21044bac447bc8f3c5f662
Koushi Vesangi <91794165+koushiv10@users.noreply.github.com>
2023-08-23 22:13:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,24 +9,26 @@ def greeting():
 @app.route('/calculator/add', methods=['POST'])
 def add_numbers():
     data = request.get_json()
-    if 'first' in data and 'second' in data:
-        first_number = data['first']
-        second_number = data['second']
-        result = first_number + second_number
-        return jsonify({'result': result}), 200
-    else:
+    first_number = data.get('first')
+    second_number = data.get('second')
+    
+    if first_number is None or second_number is None:
         return jsonify({'error': 'Both numbers are required'}), 400
+    
+    result = first_number + second_number
+    return jsonify({'result': result}), 200
 
 @app.route('/calculator/subtract', methods=['POST'])
 def subtract_numbers():
     data = request.get_json()
-    if 'first' in data and 'second' in data:
-        first_number = data['first']
-        second_number = data['second']
-        result = first_number - second_number
-        return jsonify({'result': result}), 200
-    else:
+    first_number = data.get('first')
+    second_number = data.get('second')
+    
+    if first_number is None or second_number is None:
         return jsonify({'error': 'Both numbers are required'}), 400
+    
+    result = first_number - second_number
+    return jsonify({'result': result}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
