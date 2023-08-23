9d9de75cedd7cf7683e63b5c7d8d9de0d3333fa9
Koushi Vesangi <91794165+koushiv10@users.noreply.github.com>
2023-08-23 22:31:01 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,27 +2,31 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/caluclator/greeting', methods=['GET'])
+@app.route('/calculator/greeting', methods=['GET'])
 def greeting():
     return "Hello world!", 200
 
 @app.route('/calculator/add', methods=['POST'])
 def add_numbers():
-    data = request.get_json()
-    first_number = data['first']
-    second_number = data['second']
-    
-    result = first_number + second_number
-    return jsonify({'result': result}), 200
+    try:
+        data = request.get_json()
+        first_number = data['first']
+        second_number = data['second']
+        result = first_number + second_number
+        return jsonify({'result': result}), 200
+    except (KeyError, TypeError):
+        return jsonify({'error': 'Invalid input'}), 400
 
 @app.route('/calculator/subtract', methods=['POST'])
 def subtract_numbers():
-    data = request.get_json()
-    first_number = data['first']
-    second_number = data['second']
-    
-    result = first_number - second_number
-    return jsonify({'result': result}), 200
+    try:
+        data = request.get_json()
+        first_number = data['first']
+        second_number = data['second']
+        result = first_number - second_number
+        return jsonify({'result': result}), 200
+    except (KeyError, TypeError):
+        return jsonify({'error': 'Invalid input'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
