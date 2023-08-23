8db1887c7414d789ab08dfce9f37c78821b4dfff
Koushi Vesangi <91794165+koushiv10@users.noreply.github.com>
2023-08-23 22:04:18 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,33 +2,25 @@ from flask import Flask, request, jsonify
 
 app = Flask(_name_)
 
-@app.route('/calculator/greeting', methods=['GET'])
+@app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello world!'
+    return "Hello world!"
 
-@app.route('/calculator/add', methods=['POST'])
+@app.route("/calculator/add", methods=['POST'])
 def add():
-    data = request.json
-    first = data.get('first')
-    second = data.get('second')
-    
-    if first is None or second is None:
-        return jsonify({"error": "Both 'first' and 'second' must be provided"}), 400
-    
+    data = request.get_json()
+    first = data.get('first', 0)
+    second = data.get('second', 0)
     result = first + second
-    return jsonify({"result": result})
+    return jsonify({"result": result}), 200
 
-@app.route('/calculator/subtract', methods=['POST'])
+@app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data = request.json
-    first = data.get('first')
-    second = data.get('second')
-    
-    if first is None or second is None:
-        return jsonify({"error": "Both 'first' and 'second' must be provided"}), 400
-    
+    data = request.get_json()
+    first = data.get('first', 0)
+    second = data.get('second', 0)
     result = first - second
-    return jsonify({"result": result})
+    return jsonify({"result": result}), 200
 
 if _name_ == '_main_':
     app.run(port=8080, host='0.0.0.0')
