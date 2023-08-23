a6a3d67e1e97ff10d63e274a3f495d5b6df8effc
Koushi Vesangi <91794165+koushiv10@users.noreply.github.com>
2023-08-23 22:33:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,29 +4,29 @@ app = Flask(__name__)
 
 @app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return "Hello world!", 200
+    return 'Hello world!', 200
 
 @app.route('/calculator/add', methods=['POST'])
-def add_numbers():
-    try:
-        data = request.get_json()
+def add():
+    data = request.get_json()
+    if 'first' in data and 'second' in data:
         first_number = data['first']
         second_number = data['second']
         result = first_number + second_number
         return jsonify({'result': result}), 200
-    except (KeyError, TypeError):
-        return jsonify({'error': 'Invalid input'}), 400
+    else:
+        return 'Missing parameters', 400
 
 @app.route('/calculator/subtract', methods=['POST'])
-def subtract_numbers():
-    try:
-        data = request.get_json()
+def subtract():
+    data = request.get_json()
+    if 'first' in data and 'second' in data:
         first_number = data['first']
         second_number = data['second']
         result = first_number - second_number
         return jsonify({'result': result}), 200
-    except (KeyError, TypeError):
-        return jsonify({'error': 'Invalid input'}), 400
+    else:
+        return 'Missing parameters', 400
 
 if __name__ == '__main__':
     app.run(debug=True)
